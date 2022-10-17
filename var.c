0d9f1038788f5422445a4f9f728738c9d0083348
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-18 01:43:09 +0200

    m

--- var.c
+++ var.c
@@ -10,25 +10,29 @@ int sum(int a, int b)
 // a is argument tno 1
 int add(int n, int a, ...)
 {
-    va_list num; //declare
-    va_start(num, n); //initializing
+    va_list arg; //declare
+    va_start(arg, a); //initializing
 
     int i;
-    int answer = 0;
-
+    int answer = a;
+    // int v = a;
     for(i=1; i<n; i++)
     {
-        answer = answer + va_arg(num, int);
+        answer = answer + va_arg(arg, int);
     }
 
-    va_end(num);
+    va_end(arg);
     return answer;
 }
 
 int main(void)
 {
     printf("sum = %d\n", sum(4, 3));
-    printf("add = %d\n", add(3, 3, 2, 6));
+    printf("add = %d\n", add(3, 7, 2, 6));
+    printf("add = %d\n", add(4, 12, 2, 150, -2));
+    printf("add = %d\n", add(5, 5, 7, 9, 10, 11));
+    printf("add = %d\n", add(2, 28, 14));
+
     return 0;
 }
 
