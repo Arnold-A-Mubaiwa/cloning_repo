3477d6bac3bb230f3d53adf6f6f504c1a336f1e7
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-18 01:03:18 +0200

    m

--- var.c
+++ var.c
@@ -6,8 +6,27 @@ int sum(int a, int b)
     return (a + b);
 }
 
+// n = number of arguments to be in the function
+// a is argument tno 1
+int add(int n, int a, ...)
+{
+    va_list num;
+    va_start(num, a);
+
+    int i;
+    int answer = a;
+
+    for(i=1; i<n; i++)
+    {
+        answer = va_arg(num, int);
+    }
+
+    va_end(num);
+    return answer;
+}
 int main(void)
 {
-    printf("%d", sum(2, 3));
+    printf("%d", sum(4, 3));
+    printf("%d", add(4, 3, 2, 6));
     return 0;
 }
